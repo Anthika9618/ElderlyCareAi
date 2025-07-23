c2308c3afecc7f6d00fe5f713389a8d37f49bc3e
Anthika Puttharak <anthika.puttharak@gmail.com>
2025-07-23 17:01:49 +0700

    Remove all tracked files

--- test_load_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-model_path = "/mnt/c/ElderlyCareSystem/models/falldetect_bi_lstm_testmodel.h5"
-print("กำลังโหลดโมเดลจาก:", model_path)
-
-# load model โดยไม่ compile เพื่อลดปัญหาที่อาจเกิดขึ้นตอนโหลดโมเดลเก่า
-model = tf.keras.models.load_model(model_path, compile=False)
-
-print("โหลดโมเดลสำเร็จแล้ว")
